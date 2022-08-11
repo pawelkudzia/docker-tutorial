bd9d8f457d1288d4cc335b99c38a927b5318864c
Pawel <74410157+pawelkudzia@users.noreply.github.com>
2022-08-11 10:53:51 +0200

    Add logging in ApiTestController

--- WebService/src/WebService.Api/Controllers/ApiTestController.cs
+++ WebService/src/WebService.Api/Controllers/ApiTestController.cs
@@ -6,9 +6,18 @@ namespace WebService.Api.Controllers;
 [Route("/api")]
 public class ApiTestController : ControllerBase
 {
+    private readonly ILogger<ApiTestController> _logger;
+
+    public ApiTestController(ILogger<ApiTestController> logger)
+    {
+        _logger = logger;
+    }
+
     [HttpGet("test")]
     public async Task<IActionResult> Test()
     {
+         _logger.LogInformation("[{}] Logger is working! Path: {}", DateTime.UtcNow, Request.Path);
+
         var apiTestDto = new
         {
             Message = $"API is working! Path: {Request.Path}",
